da1befe8b6aa4a7d2183f096726e3f944bc81f17
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-03-05 17:39:58 +0100

    Create file for each new version in filesystemdatastore.

--- src/main/java/org/dainst/chronontology/store/FileSystemDatastore.java
+++ src/main/java/org/dainst/chronontology/store/FileSystemDatastore.java
@@ -2,14 +2,19 @@ package org.dainst.chronontology.store;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.FileFileFilter;
 import org.apache.log4j.Logger;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Key value store where the values are of type JsonNode
@@ -21,7 +26,7 @@ public class FileSystemDatastore implements Datastore {
 
     final static Logger logger = Logger.getLogger(FileSystemDatastore.class);
 
-    private static final String SUFFIX = ".txt";
+    private static final String EXT = ".txt";
     private final String baseFolder;
 
     public FileSystemDatastore(String baseFolder) {
@@ -36,11 +41,17 @@ public class FileSystemDatastore implements Datastore {
             final String typeName,
             final String key) {
 
+        File dir= dirPath(typeName,key).toFile();
+        if (!dir.exists()) return null;
+        File latest= getLatest(dir);
+        if (latest==null) return null;
+
         String content= "";
         try {
             content = new String(Files.readAllBytes(
-                    filePath(typeName,key)));
+                    filePath(typeName,key,latest.getName())));
         } catch (IOException e) {
+            logger.error(e.getMessage());
             return null;
         }
 
@@ -48,22 +59,71 @@ public class FileSystemDatastore implements Datastore {
         try {
             n = new ObjectMapper().readTree(content);
         } catch (IOException e) {
+            logger.error(e.getMessage());
             return null;
         }
         return n;
     };
 
-    private Path filePath(final String typeName, final String key) {
-        return Paths.get(dirPath(typeName,key).toString(),"1" + SUFFIX);
+    private Path filePath(final String typeName, final String key,String filename) {
+        return Paths.get(dirPath(typeName,key).toString(),filename);
     }
 
     private Path dirPath(final String typeName,final String key) {
         return Paths.get(baseFolder,typeName,key);
     }
 
+
     /**
-     * TODO create type name dir if it does not exist
+     * Scans the dir for files matching the number + extension
+     * pattern and returns the file with the highest number in its
+     * filename.
      *
+     * @param dir
+     * @return null if no files matching the pattern are found.
+     */
+    private File getLatest(File dir) {
+        File[] files=
+            dir.listFiles(new FilenameFilter() {
+                    public boolean accept( File dir, String name ) {
+                        return name.matches( "\\d+\\"+EXT );
+                    }}
+                );
+
+        Arrays.sort(files, new Comparator<File>() {
+
+            @Override
+            public int compare(File o1, File o2) {
+                int n1 = extractNumber(o1);
+                int n2 = extractNumber(o2);
+                return n1 - n2;
+            }
+
+            private int extractNumber(File f) {
+                int nr= Integer.parseInt(FilenameUtils.getBaseName(f.getName()));
+                return nr;
+            }
+        });
+
+        if (files.length==0) return null;
+        return files[files.length-1];
+    }
+
+    /**
+     * Takes a file which is assumed to follow the number + extension pattern,
+     * and derives a new filename by taking the basename of <code>latest</code>
+     * and incrementing its number by one.
+     *
+     * @param latest
+     * @return derived filename.
+     */
+    private String oneUp(File latest) {
+        if (latest==null) return "1"+EXT;
+        int num= Integer.parseInt(FilenameUtils.getBaseName(latest.getName()));
+        return ++num+EXT;
+    }
+
+    /**
      * Stores or updates the item for key.
      * @param key
      * @param value
@@ -73,12 +133,13 @@ public class FileSystemDatastore implements Datastore {
             String key,
             JsonNode value) {
 
-        if (!dirPath(typeName,key).toFile().exists()) {
-            dirPath(typeName,key).toFile().mkdirs();
+        File dir= dirPath(typeName,key).toFile();
+        if (!dir.exists()) {
+            dir.mkdirs();
         }
 
         try {
-            Files.write( filePath(typeName,key)
+            Files.write( filePath(typeName,key,oneUp(getLatest(dir)))
                     , value.toString().getBytes());
         } catch (IOException e) {
             logger.error(e.getMessage());
@@ -94,7 +155,7 @@ public class FileSystemDatastore implements Datastore {
 
     @Override
     public boolean isConnected() {
-        // TODO This will be sufficient for now.
+        // This is sufficient for the moment.
         return true;
     }
 }
--- src/test/java/org/dainst/chronontology/store/FilesystemDatastoreTest.java
+++ src/test/java/org/dainst/chronontology/store/FilesystemDatastoreTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
 import org.dainst.chronontology.JsonTestUtils;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -21,8 +22,8 @@ public class FilesystemDatastoreTest {
     private static final String BASE_FOLDER = "src/test/resources/datastoretest/";
     private static final FileSystemDatastore store = new FileSystemDatastore(BASE_FOLDER);
 
-    @AfterClass
-    public static void afterClass() throws IOException {
+    @AfterMethod
+    public static void after() throws IOException {
         FileUtils.deleteDirectory(new File(BASE_FOLDER));
     }
 
@@ -30,5 +31,37 @@ public class FilesystemDatastoreTest {
     public void putAndGet() throws IOException {
         store.put(TYPE_NAME,"a", JsonTestUtils.sampleDocument("a"));
         assertEquals(store.get(TYPE_NAME,"a"), JsonTestUtils.sampleDocument("a"));
+    }
+
+    @Test
+    public void putAndGet2() throws IOException {
+        store.put(TYPE_NAME,"a", JsonTestUtils.sampleDocument("1"));
+        store.put(TYPE_NAME,"a", JsonTestUtils.sampleDocument("2"));
+        assertEquals(store.get(TYPE_NAME,"a"), JsonTestUtils.sampleDocument("2"));
+    }
+
+    @Test
+    public void putAndGetAndIgnoreNonParsableNumberedFiles() throws IOException {
+        store.put(TYPE_NAME,"a", JsonTestUtils.sampleDocument("1"));
+        new File(BASE_FOLDER+TYPE_NAME+"/a/a.txt").createNewFile();
+        assertEquals(store.get(TYPE_NAME,"a"), JsonTestUtils.sampleDocument("1"));
+    }
+
+    @Test
+    public void ignoredFile() throws IOException {
+        new File(BASE_FOLDER+TYPE_NAME+"/a/").mkdirs();
+        new File(BASE_FOLDER+TYPE_NAME+"/a/a.txt").createNewFile();
+        assertEquals(store.get(TYPE_NAME,"a"), null);
+    }
+
+    @Test
+    public void notexistingFile() throws IOException {
+        new File(BASE_FOLDER+TYPE_NAME+"/a/").mkdirs();
+        assertEquals(store.get(TYPE_NAME,"a"), null);
+    }
+
+    @Test
+    public void notexistingDir() throws IOException {
+        assertEquals(store.get(TYPE_NAME,"a"), null);
     }
 }
