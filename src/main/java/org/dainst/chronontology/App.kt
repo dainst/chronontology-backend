7ee0b5c069e502f0d4f7d49f3f51c47917b95883
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-15 21:26:59 +0200

    convert app to kotlin

---
+++ src/main/java/org/dainst/chronontology/App.kt
@@ -0,0 +1,55 @@
+package org.dainst.chronontology
+
+import org.apache.log4j.Logger
+import org.dainst.chronontology.config.*
+
+import java.util.Properties
+
+/**
+ * Main class. Handles wiring of application components.
+
+ * @author Daniel de Oliveira
+ */
+class App(controller: Controller) {
+
+    var controller: Controller? = null
+
+    init {
+        this.controller = controller
+    }
+
+    companion object {
+
+        internal val logger = Logger.getLogger(App::class.java)
+        private val DEFAULT_PROPERTIES_FILE_PATH = "config.properties"
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            AppConfigurator().configure(
+                    makeAppConfigFrom(properties(DEFAULT_PROPERTIES_FILE_PATH)))
+        }
+
+        private fun properties(path: String): Properties {
+
+            val props = PropertiesLoader.loadConfiguration(path)
+            if (props == null) {
+                logger.error("Could not load properties from file: config.properties.")
+                System.exit(1)
+            }
+            return props
+        }
+
+        private fun makeAppConfigFrom(props: Properties): AppConfig {
+
+            val appConfig = AppConfig()
+            if (appConfig.validate(props) == false) {
+                for (err in appConfig.constraintViolations) {
+                    logger.error(err)
+                }
+                System.exit(1)
+            }
+            return appConfig
+        }
+    }
+}
