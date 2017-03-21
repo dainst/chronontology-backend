eda9d46ae61041fb384e494b9c90e5c8a4dfe22d
Simon Hohl <simon.hohl@dainst.org>
2017-03-21 12:38:19 +0100

    Add MOVED_PERMANENTLY HTTP status

--- src/main/java/org/dainst/chronontology/Constants.java
+++ src/main/java/org/dainst/chronontology/Constants.java
@@ -7,13 +7,17 @@ public class Constants {
 
     // A library for getting the status codes is not used on purpose
     // to protect against dependency change.
-    public static final int HTTP_CREATED = 201;
-    public static final int HTTP_FORBIDDEN = 403;
     public static final int HTTP_OK = 200;
-    public static final int HTTP_NOT_FOUND = 404;
-    public static final int HTTP_UNAUTHORIZED = 401;
-    public static final int HTTP_INTERNAL_SERVER_ERROR = 500;
+    public static final int HTTP_CREATED = 201;
+
+    public static final int HTTP_MOVED_PERMANENTLY = 301;
+
     public static final int HTTP_BAD_REQUEST = 400;
+    public static final int HTTP_UNAUTHORIZED = 401;
+    public static final int HTTP_FORBIDDEN = 403;
+    public static final int HTTP_NOT_FOUND = 404;
+
+    public static final int HTTP_INTERNAL_SERVER_ERROR = 500;
 
     public static final String HEADER_AUTH = "Authorization";
     public static final String HEADER_CT = "Content-Type";
